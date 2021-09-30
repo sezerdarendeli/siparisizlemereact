ff1cc52292c1c15edaccddc01ccd457d57834119
microsmurfs <sezer.darendeli@gmail.com>
2021-10-01 01:14:24 +0300

    All project commit

---
+++ src/variables/general.js
@@ -0,0 +1,28 @@
+// ##############################
+// // // Tasks for TasksCard - see Dashboard view
+// #############################
+
+var bugs = [
+  "01.10.2021 01:02 -(0533 810 20 08 ) - Ahmet Darendeli  - İnkilap Mahallesi Vatan Caddesi No:56 Daire 24",
+  "01.10.2021 01:12 -(0532 810 20 08 ) - Mehmet Tunç  - İnkilap Mahallesi İlk Kurşun Caddesi No:56 Daire 24",
+  "01.10.2021 01:55 -(0533 810 20 08 ) - Bekir Deve  - İnkilap Mahallesi Şehit Onbaşı Caddesi No:56 Daire 24",
+  "01.10.2021 01:22 -(0533 810 20 08 ) - Ahmet Darendeli  - İnkilap Mahallesi Vatan Caddesi No:56 Daire 24",
+];
+var website = [
+  "01.10.2021 01:02 (0533 810 20 08 ) - Ahmet Darendeli  - İnkilap Mahallesi Vatan Caddesi No:56 Daire 24",
+  "01.10.2021 01:02 (0532 810 20 08 ) - Mehmet Tunç  - İnkilap Mahallesi İlk Kurşun Caddesi No:56 Daire 24",
+  "01.10.2021 01:02 (0533 810 20 08 ) - Bekir Deve  - İnkilap Mahallesi Şehit Onbaşı Caddesi No:56 Daire 24",
+  "01.10.2021 01:02 (0533 810 20 08 ) - Ahmet Darendeli  - İnkilap Mahallesi Vatan Caddesi No:56 Daire 24",
+];
+var server = [
+  "Lines From Great Russian Literature? Or E-mails From My Boss?",
+  "Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroit",
+  'Sign contract for "What are conference organizers afraid of?"',
+];
+
+module.exports = {
+  // these 3 are used to create the tasks lists in TasksCard - Dashboard view
+  bugs,
+  website,
+  server,
+};
